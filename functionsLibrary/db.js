3dcbfa53c405654b074863c82bbc263b5bd93a91
JitendraJSM <jitendranathswamijsm@gmail.com>
2025-06-14 15:42:42 +0530

    temp office commit

--- functionsLibrary/db.js
+++ functionsLibrary/db.js
@@ -7,32 +7,53 @@ const botWorkShiftHours = 16;
 const dayMs = 24 * 60 * 60 * 1000;
 const hourMs = 60 * 60 * 1000;
 
-const readProfilesData = async () => JSON.parse(await fs.readFile("./data/instaData/profilesData.json"));
+// ==== Profiles Data Functions ====
+const readProfilesData = async () =>
+  JSON.parse(await fs.readFile("./data/instaData/profilesData.json"));
 readProfilesData.shouldStoreState = "profilesData";
 
 const writeProfilesData = async (profilesData) => {
-  if (!profilesData || !Array.isArray(profilesData)) throw new Error(`Invalid profilesData object provided. It must be an array.`);
+  if (!profilesData || !Array.isArray(profilesData))
+    throw new Error(
+      `Invalid profilesData object provided. It must be an array.`
+    );
 
   // Ensure all profiles have userName, userDataPath properties and do not have duplicate objects in dueTasks
   profilesData.forEach((profile) => {
-    if (!profile.userName || !profile.userDataPath) throw new Error(`Invalid profile object provided. It must contain userName and userDataPath properties.`);
+    if (!profile.userName || !profile.userDataPath)
+      throw new Error(
+        `Invalid profile object provided. It must contain userName and userDataPath properties.`
+      );
     if (profile.dueTasks) {
       profile.dueTasks = utils.removeDuplicates(profile.dueTasks);
     }
   });
 
-  await fs.writeFile("./data/instaData/profilesData.json", JSON.stringify(profilesData, null, 2));
+  await fs.writeFile(
+    "./data/instaData/profilesData.json",
+    JSON.stringify(profilesData, null, 2)
+  );
   return true;
 };
 
+// ==== User Data Functions ====
 const getUserDataPathByUserName = async function (userName) {
-  const tempAllProfilesData = this?.state?.profilesData || (await readProfilesData());
+  const tempAllProfilesData =
+    this?.state?.profilesData || (await readProfilesData());
 
-  const userDataPath = tempAllProfilesData.find((profile) => profile.userName === userName)?.userDataPath;
-  if (!userDataPath) throw new Error(`User profile with userName: ${userName} not found in profilesData.`);
+  const userDataPath = tempAllProfilesData.find(
+    (profile) => profile.userName === userName
+  )?.userDataPath;
+  if (!userDataPath)
+    throw new Error(
+      `User profile with userName: ${userName} not found in profilesData.`
+    );
 
   // Check if file exists
-  if (!(await fs.pathExists(userDataPath))) throw new Error(`User data file does not exist for user: ${userProfile.userName}, at path userDataPath: ${userDataPath}`);
+  if (!(await fs.pathExists(userDataPath)))
+    throw new Error(
+      `User data file does not exist for user: ${userProfile.userName}, at path userDataPath: ${userDataPath}`
+    );
 
   return userDataPath;
 };
@@ -50,7 +71,8 @@ const getUserDataPathByUserName = async function (userName) {
  * const userData = await readUserProfileData({ userName: "john_doe", type: "agent" });
  */
 const readUserProfileData = async (userName) => {
-  if (!userName) throw new Error(`userName must be provided to read user profile data.`);
+  if (!userName)
+    throw new Error(`userName must be provided to read user profile data.`);
 
   const userDataPath = await getUserDataPathByUserName.call(this, userName);
 
@@ -61,19 +83,28 @@ const readUserProfileData = async (userName) => {
 };
 
 const writeUserProfileData = async (userData) => {
-  if (!userData || !userData.userName) throw new Error(`Invalid userData object provided. It must contain userName property.`);
+  if (!userData || !userData.userName)
+    throw new Error(
+      `Invalid userData object provided. It must contain userName property.`
+    );
 
-  const storedUserData = await readUserProfileData.call(this, userData.userName);
+  const storedUserData = await readUserProfileData.call(
+    this,
+    userData.userName
+  );
   userData = { ...storedUserData, ...userData };
 
   // Remove updateUserData task if exists
-  userData.dueTasks && (userData.dueTasks = utils.removeDuplicates(userData.dueTasks));
+  userData.dueTasks &&
+    (userData.dueTasks = utils.removeDuplicates(userData.dueTasks));
 
   // Write the user data to the file
   await fs.writeFile(userData.userDataPath, JSON.stringify(userData, null, 2));
 
   return true;
 };
+
+// Refactored until this line 14-06-2025
 
 const addDueTask = async function (userName, dueTaskObj) {
   console.log(`Adding due task for user: ${userName}`);
@@ -82,17 +113,19 @@ const addDueTask = async function (userName, dueTaskObj) {
 
   for (const profile of profilesData) {
     if (profile.userName === `${userName}`) {
-      if (!profile.dueTasks) profile.dueTasks = [];
       profile.dueTasks.push(dueTaskObj);
-      let typeOfProfile = profile.type;
-      const getDataPath = typeOfProfile === "agent" ? agentsDataPath : scrapersDataPath;
-      const dataPath = getDataPath(userName);
 
-      const userData = JSON.parse(await fs.readFile(dataPath));
+      const userData = JSON.parse(
+        await readUserProfileData.call(this, userName)
+      );
       userData.dueTasks.push(dueTaskObj);
+      // Refactored until this line 14-06-2025
 
       await writeUserProfileData.call(this, userData);
-    }
+    } else
+      throw new Error(
+        `User profile with userName: ${userName} not found in profilesData.`
+      );
   }
   await writeProfilesData(profilesData);
   console.log(`Added task for user: ${userName}`);
@@ -105,29 +138,44 @@ const removeDueTask = async function (userName, dueTaskObj) {
     if (profile.userName === userName) {
       if (profile.dueTasks) {
         // 1. find the index of dueTaskObj in profile.dueTasks
-        const index = profile.dueTasks.findIndex((task) => JSON.stringify(task) === JSON.stringify(dueTaskObj));
+        const index = profile.dueTasks.findIndex(
+          (task) => JSON.stringify(task) === JSON.stringify(dueTaskObj)
+        );
         // 2. If index is -1 then break the loop and throw error that dueTaskObj for userName is not found, else remove it
         if (index === -1) {
-          console.error(`This particular due task ${JSON.stringify(dueTaskObj)} for user: ${userName} not found in profilesData.`);
+          console.error(
+            `This particular due task ${JSON.stringify(
+              dueTaskObj
+            )} for user: ${userName} not found in profilesData.`
+          );
           break;
         }
         profile.dueTasks.splice(index, 1);
 
         // 3. Write the updated profile data to the file
         let typeOfProfile = profile.type;
-        const getDataPath = typeOfProfile === "agent" ? agentsDataPath : scrapersDataPath;
+        const getDataPath =
+          typeOfProfile === "agent" ? agentsDataPath : scrapersDataPath;
         const dataPath = getDataPath(userName);
 
         // 4. Update user-specific data file
         const userData = JSON.parse(await fs.readFile(dataPath));
         if (!userData.dueTasks) {
-          console.error(`Due tasks for user: ${userName} doesn't exists in file ${dataPath}.`);
+          console.error(
+            `Due tasks for user: ${userName} doesn't exists in file ${dataPath}.`
+          );
           break;
         }
 
-        const dueTaskIndex = userData.dueTasks.findIndex((task) => JSON.stringify(task) === JSON.stringify(dueTaskObj));
+        const dueTaskIndex = userData.dueTasks.findIndex(
+          (task) => JSON.stringify(task) === JSON.stringify(dueTaskObj)
+        );
         if (dueTaskIndex === -1) {
-          console.error(`This particular due task ${JSON.stringify(dueTaskObj)} for user: ${userName} doesn't exists in file ${dataPath}.`);
+          console.error(
+            `This particular due task ${JSON.stringify(
+              dueTaskObj
+            )} for user: ${userName} doesn't exists in file ${dataPath}.`
+          );
           break;
         }
         userData.dueTasks.splice(dueTaskIndex, 1);
@@ -147,33 +195,51 @@ const updateDatabaseOnFollow = async function (userObject) {
 
   // Neccessary Checks
   /* The creatation of automatedFollow Array is for new user only */
-  if (!this.state.currentProfile.automatedFollow) this.state.currentProfile.automatedFollow = [];
+  if (!this.state.currentProfile.automatedFollow)
+    this.state.currentProfile.automatedFollow = [];
   /* Sometimes in development you manullay unfollow a user that was previously automated followed and hence in automatedFollowed array but when script again perform automated follow on that same user it creates a duplicate array element for that same user but with different date so the below is check for that*/
-  const index = this.state.currentProfile.automatedFollow.findIndex((profile) => profile.userName === userObject.userName);
+  const index = this.state.currentProfile.automatedFollow.findIndex(
+    (profile) => profile.userName === userObject.userName
+  );
   if (index !== -1) {
     this.state.currentProfile.automatedFollow.splice(index, 1);
-    console.log(`User ${userObject.userName} already exists in automatedFollow array, removing the old entry.`);
+    console.log(
+      `User ${userObject.userName} already exists in automatedFollow array, removing the old entry.`
+    );
   }
 
   this.state.currentProfile.automatedFollow.push(userObject);
   await writeUserProfileData(this.state.currentProfile);
 
   /* The creatation of dueTasks Array is for new user only */
-  if (!this.state.currentProfile.dueTasks) this.state.currentProfile.dueTasks = [];
+  if (!this.state.currentProfile.dueTasks)
+    this.state.currentProfile.dueTasks = [];
 
   await addDueTask.call(this, this.state.currentProfile.userName, {
     parentModuleName: "instaAuto",
     actionName: "updateUserData",
     argumentsString: true,
   });
-  this.state.currentProfile.dueTasks = this.utils.removeDuplicates(this.state.currentProfile.dueTasks);
-  const currentfollowDueTask = this.state.currentProfile.dueTasks.find((task) => task.argumentsString === userObject.userName && task.actionName === "follow");
-  await removeDueTask.call(this, this.state.currentProfile.userName, currentfollowDueTask);
+  this.state.currentProfile.dueTasks = this.utils.removeDuplicates(
+    this.state.currentProfile.dueTasks
+  );
+  const currentfollowDueTask = this.state.currentProfile.dueTasks.find(
+    (task) =>
+      task.argumentsString === userObject.userName &&
+      task.actionName === "follow"
+  );
+  await removeDueTask.call(
+    this,
+    this.state.currentProfile.userName,
+    currentfollowDueTask
+  );
   console.log(`updateDatabaseOnFollow function completed.`);
 };
 
 const addNewProfile = async function () {
-  let userInput = await this.utils.askUser(`Do you want to add new profile? (y/n): `);
+  let userInput = await this.utils.askUser(
+    `Do you want to add new profile? (y/n): `
+  );
   if (userInput.toLowerCase() === "n") {
     return false;
   }
@@ -182,14 +248,30 @@ const addNewProfile = async function () {
   const newProfile = {};
 
   newProfile.profileTarget = await this.utils.askUser("Enter profile target: ");
-  if (this.state.profilesData.find((profile) => profile.profileTarget == newProfile.profileTarget)) throw new Error(`Profile with target ${newProfile.profileTarget} already exists`);
+  if (
+    this.state.profilesData.find(
+      (profile) => profile.profileTarget == newProfile.profileTarget
+    )
+  )
+    throw new Error(
+      `Profile with target ${newProfile.profileTarget} already exists`
+    );
 
   newProfile.userName = await this.utils.askUser(`Enter user name:`);
-  if (this.state.profilesData.find((profile) => profile.userName == newProfile.userName)) throw new Error(`Profile with user name: ${newProfile.userName} already exists`);
+  if (
+    this.state.profilesData.find(
+      (profile) => profile.userName == newProfile.userName
+    )
+  )
+    throw new Error(
+      `Profile with user name: ${newProfile.userName} already exists`
+    );
 
   newProfile.password = await this.utils.askUser(`Enter password:`);
 
-  userInput = await this.utils.askUser("Enter type of profile: 1 for 'agent', 2 for 'scrper'");
+  userInput = await this.utils.askUser(
+    "Enter type of profile: 1 for 'agent', 2 for 'scrper'"
+  );
   if (userInput === "1") newProfile.type = "agent";
   else if (userInput === "2") newProfile.type = "scraper";
   else throw new Error(`Invalid input`);
@@ -244,9 +326,12 @@ const addNewProfile = async function () {
 
 const filterProfilesToAutomate = async function () {
   // As agent's dueTasks are more important than scraper's dueTasks.
-  this.state.profilesToLoop = this.state.profilesData.filter((profile) => profile.type === "agent");
+  this.state.profilesToLoop = this.state.profilesData.filter(
+    (profile) => profile.type === "agent"
+  );
   // console.log(this.state.profilesToLoop);
-  if (this.state.profilesToLoop.length === 0) throw new Error(`No profiles to loop`);
+  if (this.state.profilesToLoop.length === 0)
+    throw new Error(`No profiles to loop`);
 };
 
 // === Interface ===
