c42ad80d5a1b3307ff80769d05d3e647abcdb129
unknown <jitendranathswamijsm@gmail.com>
2025-08-10 23:52:48 +0530

    Everything is working well but some picese are missing let's try to find them, 1. refactor and modularize db.js

---
+++ _unusedCode/temp.js
@@ -0,0 +1,43 @@
+// async function main() {
+//   const db = require("./functionsLibrary/db.js");
+//   const dueTaskObj = { parentModuleName: "instaAuto", actionName: "updateUserData", argumentsString: true };
+//   await db.removeDueTask("kajalmahioffical143", dueTaskObj);
+//   console.log("Due task removed successfully.");
+// }
+// main();
+// ======== Testing readUserProfileData() function ========
+// async function main() {
+//   const db = require("./functionsLibrary/db.js");
+//   const userName = "best.frnds.jsm";
+//   const result = await db.readUserProfileData(userName);
+//   console.log("Result is as Below:");
+//   console.log(result);
+// }
+// main();
+
+// ======== utils.removeDuplicates of writeProfilesData() Testing ========
+// const db = require("./functionsLibrary/db.js");
+// async function main() {
+//   const profilesData = await db.readProfilesData();
+//   const result = await db.writeProfilesData(profilesData);
+//   console.log(`result.`);
+// }
+
+// main();
+
+// ===== addDueTask() & removeDueTask() Testing ====
+// const db = require("./functionsLibrary/db.js");
+// async function main() {
+//   // const result = await db.addDueTask("kajalmahioffical143", {
+//   const result = await db.removeDueTask("kajalmahioffical143", {
+//     parentModuleName: "instaAuto",
+//     actionName: "updateUserData",
+//     argumentsString: true,
+//   });
+//   console.log(`result.`);
+//   console.log(result);
+// }
+
+// main();
+
+// ==== For Development of Scraper  ====
